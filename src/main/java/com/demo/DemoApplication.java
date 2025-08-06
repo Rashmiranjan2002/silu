d3b03e3fb25dfaac56fe362494dd3739b3144ac5
Rashmiranjan2002 <147078988+Rashmiranjan2002@users.noreply.github.com>
2025-08-06 23:01:43 +0530

    Update DemoApplication.java

--- src/main/java/com/demo/DemoApplication.java
+++ src/main/java/com/demo/DemoApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
